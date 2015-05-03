78c776598734b1702ace87728b4defcc3c61fdc7
frcugy <francois.cugy@gmail.com>
2015-05-03 15:05:20 +0200

    bobo

--- src/aff_brick.c
+++ src/aff_brick.c
@@ -6,7 +6,7 @@
 /*   By: frcugy <frcugy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/02 13:02:40 by frcugy            #+#    #+#             */
-/*   Updated: 2015/05/03 13:01:06 by frcugy           ###   ########.fr       */
+/*   Updated: 2015/05/03 15:04:01 by frcugy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,30 +113,30 @@ char		**aff_brick(char **map)
 char		**ft_check(char **tab, t_circlePos *movement)
 {
 	int		i = 0;
-		printf("mov.ypos = %f\n  posb.ypos_b = %f\n mov.xpos = %f\n posb.xpos = %f\n posb.xpos -f = %f\n posb.f%f\n", movement->yPos, posb.ypos_b, movement->xPos, posb.xpos_b, posb.ypos_b - posb.f, posb.f);
+		printf("mov.ypos = %f\n  posb.ypos_b = %f\n mov.xpos = %f\n posb.xpos = %f\n posb.xpos -f = %f\n posb.f%f\n posb.e %f\n", movement->yPos, posb.ypos_b, movement->xPos, posb.xpos_b, posb.ypos_b - posb.f, posb.f, (posb.e/100)*90);
 
-	if (movement->yPos <= -posb.ypos_b && movement->yPos >= -posb.ypos_b -posb.e &&movement->xPos <= -posb.xpos_b && movement->xPos >= -posb.xpos_b -posb.f)
+	if (movement->yPos <= -posb.ypos_b && movement->yPos >= -posb.ypos_b -(posb.f/100)*10 &&movement->xPos <= -posb.xpos_b && movement->xPos >= -posb.xpos_b -posb.e)
 	{
 		i = 4;
 		calculColision2(&movement, i);
 		tab[posb.i][posb.b] = tab[posb.i][posb.b] - 1;
 	}
-	else if (movement->xPos <= -posb.xpos_b && movement->xPos >= -posb.xpos_b -posb.f &&movement->yPos <= -posb.ypos_b && movement->yPos >= -posb.ypos_b -posb.e)
-	{
-		i = 1;
-		calculColision(movement, i);
-		tab[posb.i][posb.b] = tab[posb.i][posb.b] - 1;
-	}
-	else if (movement->xPos <= -posb.xpos_b - posb.f && movement->xPos >= -posb.xpos_b && movement->yPos <= -posb.ypos_b && movement->xPos >= -posb.ypos_b -posb.e)
+	if (movement->xPos <= -posb.xpos_b && movement->xPos >= -posb.xpos_b -(posb.e/100)*10 &&movement->yPos <= -posb.ypos_b && movement->yPos >= -posb.ypos_b -posb.f)
 	{
 		i = 3;
-		calculColision(movement, i);
+		calculColision2(&movement, i);
 		tab[posb.i][posb.b] = tab[posb.i][posb.b] - 1;
 	}
-	else if (movement->yPos <= -posb.ypos_b - posb.e && movement->yPos >= -posb.ypos_b &&movement->xPos <= -posb.xpos_b && movement->xPos >= -posb.xpos_b -posb.f)
+	if (movement->xPos >= -posb.xpos_b - posb.e && movement->xPos <= -posb.xpos_b -((posb.e/100)*90) && movement->yPos <= -posb.ypos_b && movement->yPos >= -posb.ypos_b -posb.f)
+	{
+		i = 1;
+		calculColision2(&movement, i);
+		tab[posb.i][posb.b] = tab[posb.i][posb.b] - 1;
+	}
+	if (movement->yPos >= -posb.ypos_b - posb.f && movement->yPos <= -(posb.ypos_b/100)*90 &&movement->xPos <= -posb.xpos_b && movement->xPos >= -posb.xpos_b -posb.e)
 	{
 		i = 2;
-		calculColision(movement, i);
+		calculColision2(&movement, i);
 		tab[posb.i][posb.b] = tab[posb.i][posb.b] - 1;
 	}
 	return (tab);
@@ -145,8 +145,8 @@ char		**ft_check(char **tab, t_circlePos *movement)
 char		**check_colision_map(char **map, t_circlePos *movement)
 {
 	posb.i = 0;
-	posb.e = ((float)2/posb.bmax);
-	posb.f = ((float)1/posb.imax);
+	posb.e = (((float)2/posb.bmax)/100)*98;
+	posb.f = (((float)1/posb.imax)/100)*90;
 	while (map[posb.i][0] != '\0')
 	{
 		posb.b = 0;
